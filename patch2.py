2db83c0d56ad225152f7ce46d27342a46a87f25f
17thedevv <traluong196@gmail.com>
2026-07-19 21:17:58 +0700

    chore: remove junk files, empty dirs, and legacy sources; update .gitignore

--- patch2.py
+++
@@ -1,6 +0,0 @@
-
-import sys
-content = open('src/MiddleEnd/TypeChecker.cpp', 'r', encoding='utf-8').read()
-content = content.replace('        void visit(DeferStmtNode& node) override { node.call->accept(*this); }\n', '')
-with open('src/MiddleEnd/TypeChecker.cpp', 'w', encoding='utf-8') as f:
-    f.write(content)
--- test2.c
+++
@@ -1,5 +0,0 @@
-int printf(const char* fmt, ...);
-int main() {
-    printf("Hello C No Headers\n");
-    return 0;
-}
